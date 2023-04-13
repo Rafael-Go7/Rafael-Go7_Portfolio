693fd7388fbdbb31a0ce48e940473f3a26f9af11
Rafael-Go7 <aixp@outlook.de>
2023-04-13 16:38:31 +0200

    ObjectComponents added + logos in IMG directory saved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { SkillsSetComponent } from './skills-set/skills-set.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    FrontPageComponent,
+    SkillsSetComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
