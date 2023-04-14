129790aeb5d23a8b60605c160ed00720801b943b
Rafael-Go7 <aixp@outlook.de>
2023-04-14 18:30:45 +0200

    SkillsSet - aus Array iterieren funktioniert

--- src/app/3_skills-set/skills-set.component.ts
+++ src/app/3_skills-set/skills-set.component.ts
@@ -6,28 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills-set.component.scss']
 })
 export class SkillsSetComponent implements OnInit{
-  // let icons_IMG: Array<IMG>;
-  icons_IMG = ['logo_1_Angular_white.svg',
-               'logo_2_Typescript_white.svg',
-               'logo_3_JavaScript_white.svg',
-                'logo_4_Html_white.svg',
-                'logo_5_Scrum_white.svg',
-                'logo_6_Firebase_white.svg',
-                'logo_7_git_white.svg',
-                'logo_8_CSS_white.svg',
-                'logo_9_API_white.svg',
-                'logo_10_materialDesign_white.svg'];
+  skills_row1 = [{'img':'logo_1_Angular_white.svg',
+                'skillDescription':'Angular'
+              },
+              {'img':'logo_2_Typescript_white.svg',
+              'skillDescription':'Typescript'
+            },
+              {'img':'logo_3_JavaScript_white.svg',
+              'skillDescription':'Javascript'
+            },
+              {'img':'logo_4_Html_white.svg',
+                'skillDescription':'HTML'
+            },
+              {'img':'logo_5_Scrum_white.svg',
+                'skillDescription':'Scrum'
+              }];
+
+  skills_row2 = [{'img':'logo_6_Firebase_white.svg',
+              'skillDescription':'Firebase'
+            },
+              {'img':'logo_7_git_white.svg',
+                'skillDescription':'Git'
+            },
+              {'img':'logo_8_CSS_white.svg',
+                'skillDescription':'CSS'
+            },
+              {'img':'logo_9_API_white.svg',
+                'skillDescription':'API'
+            },
+              {'img':'logo_10_materialDesign_white.svg',
+                'skillDescription':'Material Design'
+              }];
+
+  // skillDescription = ['Angular', 'Typescript', 
+  //                     'Javascript', 'HTML',
+  //                     'Scrum', 'Firebase',
+  //                     'Git', 'CSS', 'API',
+  //                     'Material Design'];
 
 ngOnInit() {
-
-  this.generateIMGs();
-
+  // this.generateIMGs();
 }
 
-generateIMGs(){
-  for (let index = 0; index < icons_IMG.length; index++) {
-    const logo_index = icons_IMG[index];
-    
-  }
-}
+// generateIMGs(){
+//   for (let index = 0; index < this.icons_IMG.length; index++) {
+//     const logo_index = this.icons_IMG[index];
+//   }
+// }
+
 }
--- src/app/4_portfolio/portfolio.component.ts
+++ src/app/4_portfolio/portfolio.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit{
+  skills_row1 = [{'img':'logo_1_Angular_white.svg',
+                  'skillDescription':'Angular'
+                },
+                {'img':'logo_2_Typescript_white.svg',
+                'skillDescription':'Typescript'
+                },
+                {'img':'logo_3_JavaScript_white.svg',
+                'skillDescription':'Javascript'
+                },
+                {'img':'logo_4_Html_white.svg',
+                  'skillDescription':'HTML'
+                },
+                {'img':'logo_5_Scrum_white.svg',
+                  'skillDescription':'Scrum'
+                }];
 
+ngOnInit() {
+}
 }
