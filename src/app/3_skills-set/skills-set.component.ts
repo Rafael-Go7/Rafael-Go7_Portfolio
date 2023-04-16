1e41531b41a0b5817b1b2f9719c381481b77a6d6
Rafael-Go7 <aixp@outlook.de>
2023-04-16 22:07:58 +0200

    Frontpage - CSS Baustellen

--- src/app/3_skills-set/skills-set.component.ts
+++ src/app/3_skills-set/skills-set.component.ts
@@ -6,68 +6,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills-set.component.scss']
 })
 export class SkillsSetComponent implements OnInit{
-  // skills_row1 = [{'img':'logo_1_Angular_white.svg',
-  //               'skillDescription':'Angular'
-  //             },
-  //             {'img':'logo_2_Typescript_white.svg',
-  //             'skillDescription':'Typescript'
-  //           },
-  //             {'img':'logo_3_JavaScript_white.svg',
-  //             'skillDescription':'Javascript'
-  //           },
-  //             {'img':'logo_4_Html_white.svg',
-  //               'skillDescription':'HTML'
-  //           },
-  //             {'img':'logo_5_Scrum_white.svg',
-  //               'skillDescription':'Scrum'
-  //             }];
-
-  // skills_row2 = [{'img':'logo_6_Firebase_white.svg',
-  //             'skillDescription':'Firebase'
-  //           },
-  //             {'img':'logo_7_git_white.svg',
-  //               'skillDescription':'Git'
-  //           },
-  //             {'img':'logo_8_CSS_white.svg',
-  //               'skillDescription':'CSS'
-  //           },
-  //             {'img':'logo_9_API_white.svg',
-  //               'skillDescription':'API'
-  //           },
-  //             {'img':'logo_10_materialDesign_white.svg',
-  //               'skillDescription':'Material Design'
-  //             }];
-
-skills_row1 = [{'img':'logo_1_Angular_white.svg',
-              'skillDescription':'Angular'
+  skills_row1 = [{'img':'logo_1_Angular_white.svg',
+                'skillDescription':'Angular'
+              },
+              {'img':'logo_2_Typescript_white.svg',
+              'skillDescription':'Typescript'
             },
-            {'img':'logo_2_Typescript_white.svg',
-            'skillDescription':'Typescript'
-          },
-            {'img':'logo_3_JavaScript_white.svg',
-            'skillDescription':'Javascript'
-          },
-            {'img':'logo_4_Html_white.svg',
-              'skillDescription':'HTML'
-          },
-            {'img':'logo_5_Scrum_white.svg',
-              'skillDescription':'Scrum'
+              {'img':'logo_3_JavaScript_white.svg',
+              'skillDescription':'Javascript'
             },
-            {'img':'logo_6_Firebase_white.svg',
-            'skillDescription':'Firebase'
-          },
-            {'img':'logo_7_git_white.svg',
-              'skillDescription':'Git'
-          },
-            {'img':'logo_8_CSS_white.svg',
-              'skillDescription':'CSS'
-          },
-            {'img':'logo_9_API_white.svg',
-              'skillDescription':'API'
-          },
-            {'img':'logo_10_materialDesign_white.svg',
-              'skillDescription':'Material Design'
-            }];
+              {'img':'logo_4_Html_white.svg',
+                'skillDescription':'HTML'
+            },
+              {'img':'logo_5_Scrum_white.svg',
+                'skillDescription':'Scrum'
+              }];
+
+  skills_row2 = [{'img':'logo_6_Firebase_white.svg',
+              'skillDescription':'Firebase'
+            },
+              {'img':'logo_7_git_white.svg',
+                'skillDescription':'Git'
+            },
+              {'img':'logo_8_CSS_white.svg',
+                'skillDescription':'CSS'
+            },
+              {'img':'logo_9_API_white.svg',
+                'skillDescription':'API'
+            },
+              {'img':'logo_10_materialDesign_white.svg',
+                'skillDescription':'Material Design'
+              }];
+
+// skills_row1 = [{'img':'logo_1_Angular_white.svg',
+//               'skillDescription':'Angular'
+//             },
+//             {'img':'logo_2_Typescript_white.svg',
+//             'skillDescription':'Typescript'
+//           },
+//             {'img':'logo_3_JavaScript_white.svg',
+//             'skillDescription':'Javascript'
+//           },
+//             {'img':'logo_4_Html_white.svg',
+//               'skillDescription':'HTML'
+//           },
+//             {'img':'logo_5_Scrum_white.svg',
+//               'skillDescription':'Scrum'
+//             },
+//             {'img':'logo_6_Firebase_white.svg',
+//             'skillDescription':'Firebase'
+//           },
+//             {'img':'logo_7_git_white.svg',
+//               'skillDescription':'Git'
+//           },
+//             {'img':'logo_8_CSS_white.svg',
+//               'skillDescription':'CSS'
+//           },
+//             {'img':'logo_9_API_white.svg',
+//               'skillDescription':'API'
+//           },
+//             {'img':'logo_10_materialDesign_white.svg',
+//               'skillDescription':'Material Design'
+//             }];
 
 ngOnInit() {
   // this.generateIMGs();
