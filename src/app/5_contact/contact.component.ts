96dbff8e4151bd0d43ed818c5eaa2a482c9dc45c
Rafael-Go7 <aixp@outlook.de>
2023-04-18 20:27:20 +0200

    contact.ts - php configured

--- src/app/5_contact/contact.component.ts
+++ src/app/5_contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,10 +7,56 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit { 
-
-
+  
+  @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+  @ViewChild('sendButton') sendButton!: ElementRef;
 
   ngOnInit(): void {}
+
+  async sendMail(){
+    console.log('Sending Mail', this.myForm);
+
+    let nameField =  this.nameField.nativeElement;      // rechte Seite ist Äquivalent zu document.getElementById/ wir lesen den Inhalt der Zielfelder aus
+    let emailField = this.emailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton =  this.sendButton.nativeElement;
+
+    nameField.disabled = true;          // wir deaktivieren die Zielfelder
+    emailField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+
+        //  senden //
+  let fd = new FormData();
+  fd.append('name', nameField.value);   // zu sendender Inhalt wir definiert + verpackt bzw. versandtfertig gemacht
+  fd.append('email', emailField.value);
+  fd.append('message', messageField.value);
+
+  await fetch('https://rafael-albrecht.developerakademie.net/_Backends/Portfolio/send_mail/send_mail.php'),   // Inhalt wird gesendet
+            {
+              method: 'POST',    // mit Hilfe der Post-request Methode wird versendet 
+              body: fd
+            };
+
+    // Text anzeigen Nachricht gesendet
+
+  nameField.disabled = false;
+  emailField.disabled = false;
+  messageField.disabled = false;
+  sendButton.disabled = false;
+  }
+
+onNavigate() {
+  // your logic here.... like set the url 
+  const url = 'https://rafael-albrecht.developerakademie.net/_Backends/Portfolio/send_mail/success.html';
+  window.open(url, '_blank');
+}
+
+
+// FormValidation
 
 public contactForm: FormGroup = new FormGroup({
   name: new FormControl('',[
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './0_header/header.component';
@@ -26,6 +26,7 @@ import { FooterComponent } from './6_footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
