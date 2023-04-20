f97be584374900ccfb19c20e7a74a22dc73fb37b
Rafael-Go7 <aixp@outlook.de>
2023-04-20 16:12:34 +0200

    frontPage-Component responsive Design preFinal_1

--- src/app/0_header/header.component.ts
+++ src/app/0_header/header.component.ts
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit { 
   constructor() { }
-  
+
+
   ngOnInit(): void {
+    const menuBtn = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-nav');
+    const mobileMenuLinks = document.querySelectorAll('.mobile-nav a');
+    menuBtn?.addEventListener('click', () => {
+      menuBtn.classList.toggle('is-active');
+      mobileMenu?.classList.toggle('is-active');
+    });
+    mobileMenuLinks?.forEach(link => {
+      link.addEventListener('click', () => {
+        mobileMenu?.classList.remove('is-active');
+        menuBtn?.classList.remove('is-active');
+      });
+    });
   }
 }
