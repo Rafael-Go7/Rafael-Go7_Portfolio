97b15f589f566cd6b94da99c0993a49dc55c0c3b
Rafael-Go7 <aixp@outlook.de>
2023-04-27 14:29:49 +0200

    Portfolio - AOS_effects_final via online implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';  // import router-links module
 
-import AOS from 'node_modules/aos/dist/aos.js'; // import AOS module manually
+// import AOS from 'node_modules/aos/dist/aos.js'; // import AOS module manually
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'Rafael-Go7_Portfolio';
 
 constructor(public router: Router){}    // important for the usage of router-links
 
-ngOnInit(){
-  AOS.init();   
+// ngOnInit(){
+//   AOS.init();             // locoal implementation
 
-}
+// }
 
 
 }
